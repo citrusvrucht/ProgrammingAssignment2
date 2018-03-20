8a4a00331bcabdaee89b3ed4ffad8a228b826c93
MBijsterboschABT <m.bijsterbosch@abt.eu>
2018-03-20 10:06:19 +0100

    modified functions

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,43 @@
 ## functions do
 
 ## Write a short comment describing this function
+#object containg a matrix and a field to store the inverse wich will be set by an external function
+#result of function is a list of functions to acces and modify the data
 
-makeCacheMatrix <- function(x = matrix()) {
-
+##matrix object that van cache its inverse
+makeCacheMatrix <- function(x = matrix()) 
+{
+	invMatrix <- NULL
+	set <- function(y)
+	{
+		x<<-y#set x in parent to y
+		invMatrix <<-NULL#matrix is changed, reset inverse matrix
+	}
+	get <- function() x #return x
+	setInvMatrix <- function(solve) invMatrix <<- solve #allow external function to set inv matrix
+	getInvMatrix <- function()invMatrix
+	#return result as list of function objects
+	list( set=set, get=get,setInvMatrix=setInvMatrix,getInvMatrix=getInvMatrix)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#function to add the inv matrix to the makeCachMatrix function object
+#silly structure, would be better to integrate the inv matrix function in the makeCachMatrix
+#and check if the inv != null when calling getInvMatrix, if it is null then calc, store in invMatrix and return
+#if it isnt null then return
+cacheSolve <- function(x, ...) 
+{
+	invMatrix <- x$getInvMatrix()
+	{
+		if(!is.null(invMatrix))
+		{
+			message("Getting cached inverse matrix")
+			return(invMatrix)
+		}
+		data<-x$get()
+		invMatrix <- solve(data,...)
+		x$setInvMatrix(invMatrix)
+		invMatrix
+	}
 }
